cf17c551c3d41ebfa820d73468784cea22275a08
Alexander Eide <atlexeide@gmail.com>
2023-02-08 13:05:02 +0100

    Update game end message

--- js/javascript.js
+++ js/javascript.js
@@ -12,10 +12,10 @@ function playRound(player, computer) {
     console.log("player: " + player)
     console.log("computer: " + computer)
     if (player === computer) { console.log("Draw") }
-    else if (player === "rock" && computer === "scissors") { console.log("You win!") }
-    else if (player === "paper" && computer === "rock") { console.log("You win!") }
-    else if (player === "scissors" || "scissor" && computer === "paper") { console.log("You win!") }
-    else { console.log("You lose!") }
+    else if (player === "rock" && computer === "scissors") { console.log(`You win! ${player} beats ${computer}!`) }
+    else if (player === "paper" && computer === "rock") { console.log(`You win! ${player} beats ${computer}!`) }
+    else if (player === "scissors" || "scissor" && computer === "paper") { console.log(`You win! ${player} beats ${computer}!`) }
+    else { console.log(`You lose! ${computer} beats ${player}`) }
 }
 
 
