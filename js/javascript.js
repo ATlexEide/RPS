98c4273d87bbed9d0e82d8b8a9177a0c2aebf92e
Alexander Eide <atlexeide@gmail.com>
2023-02-12 18:47:07 +0100

    add new iteration of the game It works, so its good enough for now

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,61 @@
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let number = Math.floor(Math.random() * 3) + 1;
     if (number === 1) { return 'rock'; }
     else if (number === 2) { return 'paper'; }
     else if (number === 3) { return 'scissors'; }
+    // return "paper";
 };
 
 
-function playRound(player, computer) {
-    player = player.toLowerCase();
-    console.log("player: " + player)
-    console.log("computer: " + computer)
-    if (player === computer) { console.log("Draw") }
-    else if (player === "rock" && computer === "scissors") { console.log(`You win! ${player} beats ${computer}!`) }
-    else if (player === "paper" && computer === "rock") { console.log(`You win! ${player} beats ${computer}!`) }
-    else if (player === "scissors" || "scissor" && computer === "paper") { console.log(`You win! ${player} beats ${computer}!`) }
-    else { console.log(`You lose! ${computer} beats ${player}`) }
+// Checks if the input is a valid option?
+function isValidInput(input) {
+    if (input === "rock" || input === "paper" || input === "scissors") {
+        return true;
+    } else {
+        return false;
+    }
+}
+function getInput() {
+    input = prompt("Rock, Paper or Scissors?")
+    input = input.toLowerCase()
+    console.log("getInput: " + input)
+    return input;
+}
+function playRound() {
+    const playerInput = getInput();
+    const computer = getComputerChoice();
+    if (isValidInput(playerInput)) {
+        console.log("Player: " + playerInput)
+        console.log("Computer: " + computer)
+        if (playerInput === computer) { console.log("Draw") }
+        if (playerInput === "rock") {
+            if (computer === "paper") { console.log("You lose"); computerScore++; }
+            if (computer === "scissors") { console.log("You win"); playerScore++; }
+        }
+        if (playerInput === "paper") {
+            if (computer === "rock") { console.log("You win"); playerScore++; }
+            if (computer === "scissors") { console.log("You lose"); playerScore++; }
+        }
+        if (playerInput === "scissors") {
+            if (computer === "rock") { console.log("You lose"); playerScore++; }
+            if (computer === "paper") { console.log("You win"); playerScore++; }
+        }
+    } else { alert("Please enter valid option"), game() };
+    console.log(`Score: Player[${playerScore}] : Computer [${computerScore}]`)
+    replay();
+}
+function replay() {
+    if (playerScore < 5) {
+        if (computerScore < 5) { game() }
+    }
+}
+// Play a game by calling the playRound function
+function game() {
+    playRound();
 }
 
-
-
-playRound(prompt("Rock, Paper or Scissors"), getComputerChoice())
+// Calls the game function to play the game
+game();
